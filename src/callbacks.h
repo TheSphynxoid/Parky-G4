018c94fac696f10650019707eca1b6325e1d8b1a
Hamza 'The Sphynx' Saied <hamzajamil.saied@esprit.tn>
2024-11-19 00:02:55 +0100

    First commit HS

---
+++ src/callbacks.h
@@ -0,0 +1,110 @@
+#include <gtk/gtk.h>
+
+
+void
+on_RB_annuler_bouton_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RB_confirmer_bouton_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RB_modifier_bouton_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RB_supprimer_bouton_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RB_Mois_histo_changed               (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_RB_voir_fact_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RB_payer_fact_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_buttonCO_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_BCom_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_buttonR_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_BR___clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_buttonAdd_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_buttonMod_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Rl_buttonSupp_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_Annuler_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RL_Affecter_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AA_Ajouter_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AA_modifier_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AA_supprimer_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_HS_auth_valider_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button2_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button3_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button4_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
